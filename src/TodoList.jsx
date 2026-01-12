86c8e3d8a8519f6ff59363f55d3d7ca645e5ee29
oseimacdonald <oseimacdonald@gmail.com>
2026-01-12 03:39:09 +0000

    a

---
+++ src/TodoList.jsx
@@ -0,0 +1,20 @@
+import { TodoItem } from "./TodoItem"
+
+export function TodoList({ todos, toggleTodo, deleteTodo }) {
+    return (
+        <ul className="List">
+        {todos.length === 0 && <li>No Todos</li>}
+
+        {todos.map(todo => {
+            return (
+                <TodoItem
+                {...todo} 
+                key={todo.id}
+                toggleTodo={toggleTodo}
+                deleteTodo={deleteTodo} 
+                />
+            )
+        })}
+      </ul>
+    )
+}
